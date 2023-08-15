ade7666f15bbd7172ef3130d1cb98cb3a8fede51
cobaltcyan <cobaltcyan.park@gmail.com>
2023-08-16 03:46:32 +0900

    [Fix] posting 라우터 변경

--- src/routes/postingRouter.ts
+++ src/routes/postingRouter.ts
@@ -3,13 +3,13 @@ import express from 'express';
 
 // import communityRouter from './communityRouter';
 import userController from '../controllers/userController';
-import communityController from '../controllers/postingController';
-const communityRouter = express.Router();
+import postingController from '../controllers/postingController';
+const postingRouter = express.Router();
 
-communityRouter.post('/create', communityController.postNewPosting);             // 과제 3. 게시글 작성
-communityRouter.get('/list', communityController.getAllPosting);                 // 과제 4. 게시글 목록 조회
-communityRouter.get('/info/:id', communityController.getPostingById);            // 과제 5. 특정 게시글 조회
-communityRouter.patch('/info/:id', communityController.patchPostingById);        // 과제 6. 특정 게시글 수정
-communityRouter.delete('/info/:id', communityController.deletePostingById);      // 과제 7. 특정 게시글 삭제
+postingRouter.post('/create', postingController.postNewPosting);             // 과제 3. 게시글 작성
+postingRouter.get('/list', postingController.getAllPosting);                 // 과제 4. 게시글 목록 조회
+postingRouter.get('/info/:id', postingController.getPostingById);            // 과제 5. 특정 게시글 조회
+postingRouter.patch('/info/:id', postingController.patchPostingById);        // 과제 6. 특정 게시글 수정
+postingRouter.delete('/info/:id', postingController.deletePostingById);      // 과제 7. 특정 게시글 삭제
 
-export default communityRouter;
+export default postingRouter;
