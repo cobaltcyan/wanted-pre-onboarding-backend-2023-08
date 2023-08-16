e3a123c73d29fa042761ddf8ffbca155d7a0e3fc
cobaltcyan <cobaltcyan.park@gmail.com>
2023-08-17 01:26:18 +0900

    [Comment] Posting domain 주석 정리

--- src/domain/Posting.ts
+++ src/domain/Posting.ts
@@ -1,32 +1,23 @@
 class Posting {
     id: bigint | null;
-    // userId: string;
     userId: bigint;
     title: string;
     content: string;
     hidden: Boolean | null = false;
     createdAt: Date | null;
-    // createdId: string | null;
     createdId: bigint | null;
     updatedAt: Date | null;
-    // updatedId: string | null;
     updatedId: bigint | null;
     
     constructor(
         id: bigint | null,
-        // userId: string,
         userId: bigint,
         title: string,
         content: string,
         hidden: Boolean | null = false,
-        // createdAt: Date = new Date(),
         createdAt: Date | null,
-        // createdId: string | null,
         createdId: bigint | null,
-        // updatedAt: Date | null = new Date(),
         updatedAt: Date | null,
-        // updatedAt: Date = new Date(),
-        // updatedId: string | null
         updatedId: bigint | null
     ) {
         this.id = id || null;
@@ -34,7 +25,6 @@ class Posting {
         this.title = title;
         this.content = content;
         this.hidden = hidden;
-        // this.createdAt = new Date();
         this.createdAt = createdAt;
         this.createdId = createdId;
         this.updatedAt = updatedAt;
